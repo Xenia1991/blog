3a7ce8685280b16ac67b54f1464847d1a5af1773
Xenia <andreeva19-91@mail.ru>
2024-11-24 21:28:47 +0300

    style: add styles for sign-in-form

--- src/components/sign-in-form/sign-in-form.jsx
+++ src/components/sign-in-form/sign-in-form.jsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+
 import classes from './sign-in-form.module.scss';
 
-const SignInForm = () => {};
+const SignInForm = () => {
+  return (
+    <section className={classes['sign-in-section']}>
+      <form className={classes['sign-in-form']} method="get">
+        <h3 className={classes['sign-in-form__header']}>Sign In</h3>
+        <label className={classes['sign-in-form__email-label']}>
+          <p className={classes['sign-in-form__text']}>Email address</p>
+          <input type="email" className={classes['sign-in-form__input']} placeholder="Email address" />
+        </label>
+        <label className={classes['sign-in-form__password-label']}>
+          <p className={classes['sign-in-form__text']}>Password</p>
+          <input type="password" className={classes['sign-in-form__input']} placeholder="Password" />
+        </label>
+        <button type="submit" className={classes['sign-in-form__button']}>
+          Login
+        </button>
+        <p className={classes['sign-in-form__notification']}>
+          Don&apos;t have an account?
+          <Link to="/sing-up" className={classes['sign-in-form__link']}>
+            Sign Up.
+          </Link>
+        </p>
+      </form>
+    </section>
+  );
+};
 
 export default SignInForm;
